247b4ef977d9731bb377ce97cf308da5d7598144
Shofiul <shofiul.islam@northsouth.edu>
2018-12-19 21:17:40 +0600

    Complete InsertionSort.h file

--- Header/InsertionSort.h
+++ Header/InsertionSort.h
@@ -1,44 +1,15 @@
-#include"InsertionSort.h"
+#ifndef INSERTIONSORT_H
+#define INSERTIONSORT_H
+#include<vector>
+#include<iostream>
 
-InsertionSort::InsertionSort() {
+class InsertionSort {
+public:
+	InsertionSort();
+	~InsertionSort();
+	void InsertionSorter(int[], int);
+	void InsertionSorter(std::vector<int>, int);
+};
 
-}
-InsertionSort::~InsertionSort() {
 
-}
-void InsertionSort::InsertionSorter(int arr[], int arr_size) {
-	if (arr_size > 1) {
-		int size = arr_size;
-		for (int i = 0; i < size; i++) {
-			int j = i - 1;
-			int key = arr[i];
-			while (j >= 0 && arr[j] > key) {
-				arr[j + 1] = arr[j];
-				j--;
-			}
-			arr[j + 1] = key;
-			std::cout << "Step " << i + 1 << ": ";
-			for (int i = 0; i < arr_size; i++)
-				std::cout << arr[i] << " ";
-			std::cout << std::endl;
-		}
-	}
-}
-void InsertionSort::InsertionSorter(std::vector<int> arr, int arr_size) {
-	if (arr_size > 1) {
-		int size = arr_size;
-		for (int i = 0; i < size; i++) {
-			int j = i - 1;
-			int key = arr[i];
-			while (j >= 0 && arr[j] > key) {
-				arr[j + 1] = arr[j];
-				j--;
-			}
-			arr[j + 1] = key;
-			std::cout << "Step " << i + 1 << ": ";
-			for (int i = 0; i < arr_size; i++)
-				std::cout << arr[i] << " ";
-			std::cout << std::endl;
-		}
-	}
-}
+#endif // INSERTIONSORT_H
