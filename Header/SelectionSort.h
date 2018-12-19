5e87edee4936906437232f1a720ad95b849728eb
Shofiul <shofiul.islam@northsouth.edu>
2018-12-19 22:04:00 +0600

    Complete SelettionSort.h file

---
+++ Header/SelectionSort.h
@@ -0,0 +1,14 @@
+#ifndef SELECTIONSORT_H
+#define SELECTIONSORT_H
+#include<vector>
+using namespace std;
+
+class SelectionSort {
+public:
+	SelectionSort();
+	~SelectionSort();
+	void SelectionSorter(int[], int);
+	void SelectionSorter(vector<int>, int);
+};
+
+#endif // SELECTIONSORT_H
