74226af95da9c2296417da9420067987813617bf
Shofiul <shofiul.islam@northsouth.edu>
2018-12-19 21:05:51 +0600

    Complete BubbleSort.cpp file

--- Source File/BubbleSort.cpp
+++ Source File/BubbleSort.cpp
@@ -29,3 +29,26 @@ void BubbleSort::BubbleSorter(int *array, int n) {
 
 	}
 }
+void BubbleSort::BubbleSorter(vector<int>array, int n) {
+	bool swaped = true;
+	int j = 0, temp, count = 1;
+	while (swaped) {
+		swaped = false;
+		j++;
+		for (int i = 0; i < n - j; i++) {
+			if (array[i] > array[i + 1]) {
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				swaped = true;
+			}
+			cout << "Step " << count << ": ";
+			for (int i = 0; i < n; i++) {
+				cout << array[i] << " ";
+			}
+			cout << endl;
+			count++;
+		}
+
+	}
+}
