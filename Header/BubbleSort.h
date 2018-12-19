3d0af1a2838b6cc7b6036faa6a0d0a5398f7e3ee
Shofiul <shofiul.islam@northsouth.edu>
2018-12-19 20:53:44 +0600

    wrote BubbleSort class in BubbleSort.h

--- Header/BubbleSort.h
+++ Header/BubbleSort.h
@@ -1,1 +1,10 @@
-#ifndef 
+#ifndef BUBBLESORT_H
+#define BUBBLESORT_H
+#include<iostream>
+#include<vector>
+class BubbleSort {
+	BubbleSort();
+	~BubbleSort();
+	void BubbleSorter(int*,int);
+	void BubbleSorter(vector<int>,int)
+};
